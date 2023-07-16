b44048bd0fed68bc49e8c901a9e40f9da8cc8795
Kaustubh <sherlockaustubh@gmail.com>
2023-07-16 11:12:36 +0530

    Initial Commit

---
+++ SyncPlay/SyncPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SyncPlayApp.swift
+//  SyncPlay
+//
+//  Created by Kaustubh on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SyncPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
